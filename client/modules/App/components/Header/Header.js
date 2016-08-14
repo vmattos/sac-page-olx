d14d9760c9e9e5ec6466595fa2d1fdbe94d46563
Vitor Mattos <vitor.mattos@vtex.com.br>
2016-08-14 02:24:14 -0300

    Uses grid system in header

--- client/modules/App/components/Header/Header.js
+++ client/modules/App/components/Header/Header.js
@@ -1,6 +1,7 @@
 import React, { PropTypes } from 'react';
 import { FormattedMessage } from 'react-intl';
 import { Link } from 'react-router';
+import { Row, Col } from 'react-bootstrap';
 
 // Import Style
 import styles from './Header.css';
@@ -18,14 +19,18 @@ export function Header(props, context) { // eslint-disable-line
           {languageNodes}
         </ul>
       </div>
-      <div className={styles.content}>
-        <h1 className={styles['site-title']}>
-          <FormattedMessage id="siteTitle" />
-        </h1>
-        <h3 className={styles['site-subtitle']}>
-          <FormattedMessage id="subtitle" />
-        </h3>
-      </div>
+      <Row className={styles.content}>
+        <Col sm={6}>
+          <h1 className={styles['site-title']}>
+            <FormattedMessage id="siteTitle" />
+          </h1>
+        </Col>
+        <Col sm={6}>
+          <h3 className={styles['site-subtitle']}>
+            <FormattedMessage id="subtitle" />
+          </h3>
+        </Col>
+      </Row>
     </Link>
   );
 }
